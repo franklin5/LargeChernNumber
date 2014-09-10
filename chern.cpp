4683131ce7d3fc68ac9053ed847dc80f921b96e9
franklin5 <buptphyphy@gmail.com>
2014-09-10 10:54:43 -0500

    Implemented Chern number calculation using the well known toy model

--- chern.cpp
+++ chern.cpp
@@ -23,6 +23,7 @@ void cChern::distribution(){
   int *sendcounts, *displs, *recvcounts, *displs_r;
   double  *localEig, *TotalEig;
   complex<double> chern_rank;
+  double chern_rank_real, total_chern;
   const int root = 0;
   int offset;
   //  SelfAdjointEigenSolver<MatrixXcd> ces;
@@ -58,10 +59,11 @@ void cChern::distribution(){
     }
     MPI_Barrier(COMM_WORLD);
   }
-
-  //  int MPI_Reduce(void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm);
-
+  chern_rank_real = chern_rank.real();
+  MPI_Reduce(&chern_rank_real, &total_chern, 1, MPI_DOUBLE, MPI_SUM, root, MPI_COMM_WORLD);
+  
   if (root==rank) {
+    cout << "Total Chern Number is: " << total_chern << endl;
     delete []sendbuf;
     delete []sendcounts;
     delete []displs;
@@ -76,52 +78,13 @@ void cChern::construction(){
 void cChern::update(int nk){
   if (nk == -1) {
     _bdg_H.setZero(); // This is done only once.
-    int p, q;
-    // The off-diagonal coupling introduced from time-dependent order parameter should be computed only here.
-    complex<double> Gamma2;
-    FILE *sf_inputR, *sf_inputI;
-    sf_inputR = fopen ("Rdata_2109.dat","r"); // TODO: modify input file name
-    sf_inputI = fopen ("Idata_2109.dat","r");
-    assert (sf_inputR != NULL);
-    assert (sf_inputI != NULL);
-    double dt = 0.0005;
-    double t;
-    VectorXcd Delta_t(100000);
-    Delta_t.setZero();
-    int count = 0;
-    double reD, imD;
-    //	  ofstream test_output;
-    //	  test_output.open("test_Delta.OUT"); // TODO: modify output file name
-    //	  assert(test_output.is_open());
-    while (fscanf(sf_inputR, "%lf", &reD) != EOF && fscanf(sf_inputI, "%lf", &imD) != EOF ){
-      Delta_t(count) = complex<double>(reD,imD);
-      //		test_output << reD << '\t' << imD << endl;
-      count++;
-    }
-    //	  test_output.close();
-    fclose (sf_inputR);
-    fclose (sf_inputI);
-    for (int i = 0; i < pblock; ++i) {
-      p = i-_PMAX;
-      for (int j = 0; j < pblock; ++j) {
-	q = j-_PMAX;
-	Gamma2 = complex<double> (0.0,0.0);
-	t = 0.0;
-	for (int ig = 0; ig < count; ++ig) {
-	  Gamma2 +=  std::abs(Delta_t(ig)) *
-	    complex<double> (cos(2*M_PI*(q-p)*t/_T),-sin(2*M_PI*(q-p)*t/_T));
-	  t += dt;
-	}
-	Gamma2 = Gamma2/_T*dt;
-	//	  			cout << Gamma2 << endl;
-	_bdg_H(i+2*pblock,j+pblock) = Gamma2;
-	_bdg_H(i+3*pblock,j) = -Gamma2;
-      }
-    }
+    _bdg_H(2,1) = -1.0;
+    _bdg_H(3,0) =  1.0;
+
   } else {
     int nkx = nk % _NKX;     // --> the modulo (because nk = nkx+ nky * NKX )
     int nky = int (nk/_NKX); // --> the floor
-    double kmax = 2.0; // TODO: modify momentum space cutoff value
+    double kmax = 5.0; // TODO: modify momentum space cutoff value
     double kx = -kmax + nkx * kmax *2.0 /(_NKX-1);
     double ky = -kmax + nky * kmax *2.0 /(_NKX-1);
     SelfAdjointEigenSolver<MatrixXcd> ces;
@@ -135,7 +98,7 @@ void cChern::update(int nk){
 	update_kxky(qx,qy);
 	ces.compute(_bdg_H); 
 	_loopA = ces.eigenvectors();
-	cout << "_loopA is finished." << endl;
+	//	cout << "_loopA is finished." << endl;
 	break;
       case 2:
 	qx = kx + dk;
@@ -143,7 +106,7 @@ void cChern::update(int nk){
 	update_kxky(qx,qy);
 	ces.compute(_bdg_H); 
 	_loopB = ces.eigenvectors();
-	cout << "_loopB is finished." << endl;
+	//	cout << "_loopB is finished." << endl;
 	break;
       case 3:
 	qx = kx + dk;
@@ -151,7 +114,7 @@ void cChern::update(int nk){
 	update_kxky(qx,qy);
 	ces.compute(_bdg_H); 
 	_loopC = ces.eigenvectors();
-	cout << "_loopC is finished." << endl;
+	//	cout << "_loopC is finished." << endl;
 	break;
       case 4:
 	qx = kx - dk;
@@ -159,45 +122,37 @@ void cChern::update(int nk){
 	update_kxky(qx,qy);
 	ces.compute(_bdg_H); 
 	_loopD = ces.eigenvectors();
-	cout << "_loopD is finished." << endl;
+	//	cout << "_loopD is finished." << endl;
 	break;
       default:
 	break;
       }
     }
-    int lowerbound;
-    for(int ip = 0; ip < 2*pblock;++ip){
-      if (ces.eigenvalues()[ip]/(M_PI/_T) > -1.1) {// Questionable threshold definition for all momentum values...
-	lowerbound = ip;
-	break;
-      }
-    }
-    cout << "lower bound = " << lowerbound << " upper bound = " << 2*pblock << endl; // There has got to be another way of finding lower bound for the Floquet truncation in first energy zone.
     _chern = complex<double> (1.0,0.0);
-    for(int ip = lowerbound; ip < 2*pblock; ++ip) {
-      //_temploop = _loopA.col(ip);
-      //      cout << _loopA.col(ip).adjoint() * _loopB.col(ip) << endl;
-      _chern = _chern * _loopA.col(ip).adjoint() * _loopB.col(ip);
-      _chern = _chern * _loopB.col(ip).adjoint() * _loopC.col(ip);
-      _chern = _chern * _loopC.col(ip).adjoint() * _loopD.col(ip);
-      _chern = _chern * _loopD.col(ip).adjoint() * _loopA.col(ip);
+    complex<double> temp;
+    for(int ip = 0; ip < 2; ++ip) {
+      temp = _loopA.col(ip).adjoint() * _loopB.col(ip);
+      _chern = _chern * temp/abs(temp);
+      temp = _loopB.col(ip).adjoint() * _loopC.col(ip);
+      _chern = _chern * temp/abs(temp); 
+      temp = _loopC.col(ip).adjoint() * _loopD.col(ip);
+      _chern = _chern * temp/abs(temp); 
+      temp = _loopD.col(ip).adjoint() * _loopA.col(ip);
+      _chern = _chern * temp/abs(temp); 
     }
     _chern = complex<double>(log(std::abs(_chern)),atan(_chern.imag()/_chern.real()));
-    cout << _chern << endl;
+    _chern = _chern/2/M_PI/complex<double>(0.0,1.0);
+    //    cout << _chern << endl;
   }
 }	    
 
 void cChern::update_kxky(double kx, double ky){
   double xi = kx*kx + ky*ky - _mu;
-  int p;
-  for (int i = 0; i < pblock; ++i) {
-    p = i-_PMAX;
-    // only lower left part of the matrix is needed for storage.
-    _bdg_H(i,i) 	   = complex<double>(xi+_h+2*M_PI*p/_T,0.0);
-    _bdg_H(i+pblock,i)   = complex<double>(_v*kx,_v*ky);
-    _bdg_H(i+pblock,i+pblock)= complex<double>(xi-_h+2*M_PI*p/_T,0.0);
-    _bdg_H(i+2*pblock,i+2*pblock) = complex<double>(-(xi+_h+2*M_PI*p/_T),0.0);
-    _bdg_H(i+3*pblock,i+2*pblock) = complex<double>(_v*kx,-_v*ky);
-    _bdg_H(i+3*pblock,i+3*pblock) = complex<double>(-(xi-_h+2*M_PI*p/_T),0.0);
-  }
+  // only lower left part of the matrix is needed for storage.
+    _bdg_H(0,0) = complex<double>(xi+_h,0.0);
+    _bdg_H(1,0) = complex<double>(_v*kx,-_v*ky);
+    _bdg_H(1,1) = complex<double>(xi-_h,0.0);
+    _bdg_H(2,2) = complex<double>(-(xi+_h),0.0);
+    _bdg_H(3,2) = complex<double>(_v*kx,_v*ky);
+    _bdg_H(3,3) = complex<double>(-(xi-_h),0.0);
 }
--- chern.h
+++ chern.h
@@ -25,11 +25,11 @@ public:
     _Eb(para.t), _h(para.h), _v(para.v),
     _mu(phys.mu),_T(phys.T),
     // TODO: modify frequency cutoff
-    _PMAX(40), // number of frequency cutoff for time expansion
-    pblock(2*_PMAX+1),
+    _PMAX(4), // number of frequency cutoff for time expansion
+    pblock(4),
     // TODO: modify mmtn space cutoff for the bulk system
-    pblock4(4*pblock),
-    _MomentumSpaceCutoff(50),
+    pblock4(4),
+    _MomentumSpaceCutoff(100),
     _NKX(2*_MomentumSpaceCutoff+1),
     _NKX2(_NKX*_NKX),
     _bdg_E(pblock4),
